28fd948ea3c5d8c3022f36d710cf2b1521a57040
Otim0310 <rkdmsdlaa123@naver.com>
2025-08-12 23:50:40 +0900

    Update SuccessStatus.java

--- src/main/java/com/likelion/cheongsanghoe/exception/status/SuccessStatus.java
+++ src/main/java/com/likelion/cheongsanghoe/exception/status/SuccessStatus.java
@@ -7,16 +7,14 @@ import org.springframework.http.HttpStatus;
 @Getter
 @AllArgsConstructor
 public enum SuccessStatus {
-    // SUCCESS 2XX
     SUCCESS(HttpStatus.OK, "COMMON200", "요청이 성공적으로 처리되었습니다."),
     CREATED(HttpStatus.CREATED, "COMMON201", "리소스가 성공적으로 생성되었습니다."),
 
-    // AUTH 관련 성공
+
     LOGIN_SUCCESS(HttpStatus.OK, "AUTH200", "로그인이 성공적으로 처리되었습니다."),
     LOGOUT_SUCCESS(HttpStatus.OK, "AUTH200", "로그아웃이 성공적으로 처리되었습니다."),
     USER_INFO_SUCCESS(HttpStatus.OK, "AUTH200", "사용자 정보를 성공적으로 조회했습니다."),
 
-    // MEMBER 관련 성공
     MEMBER_CREATED(HttpStatus.CREATED, "MEMBER201", "회원이 성공적으로 생성되었습니다."),
     MEMBER_INFO_SUCCESS(HttpStatus.OK, "MEMBER200", "회원 정보를 성공적으로 조회했습니다."),
     MEMBER_UPDATED(HttpStatus.OK, "MEMBER200", "회원 정보가 성공적으로 수정되었습니다."),
